1069fb4709faa98f0eb2f6fe10fd1c2ebbfa5a73
TrifonJordanov <90862281+TrifonJordanov@users.noreply.github.com>
2022-05-06 10:23:51 +0300

    Add files via upload

--- Summator.Tests/SummatorTests.cs
+++ Summator.Tests/SummatorTests.cs
@@ -4,7 +4,7 @@ namespace Summator.Tests
 {
     public class SummatorTests
     {
-        [Test]  
+        [Test]
         public void Test_Sum_TwoPossitiveNumbers()
         {
             long actual = Summator.Sum(new int[] { 5, 7 });
@@ -49,11 +49,61 @@ namespace Summator.Tests
         [Test]
         public void Test_Average_TwoPossitiveNumbers()
         {
-            int actual = Summator.Average(new int[] { 5, 7 });
+            double actual = Summator.Average(new int[] { 5, 7 });
 
-            int expected = 6;
+            double expected = 6;
 
             Assert.That(expected == actual);
+        }
+
+        [Test]
+        public void Test_Average_TwoNegativeNumbers()
+        {
+            double actual = Summator.Average(new int[] { -5, -7 });
+
+            double expected = -6;
+
+            Assert.That(expected == actual);
+        }
+
+        [Test]
+        public void Test_Average_WithOddSumAndEvenSeparator()
+        {
+            double actual = Summator.Average(new int[] { 6, 7 });
+
+            double expected = 6.5;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Test_Average_WithNegativeOddSumAndEvenSeparator()
+        {
+            double actual = Summator.Average(new int[] { -6, -7 });
+
+            double expected = -6.5;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Test_SumOddOrEven_WithOddSum()
+        {
+            string oddSum = Summator.SumOddOrEven(new int[] { 6, 7 });
+
+            string expected = "Sum result is odd!";
+
+            Assert.AreEqual(expected, oddSum);
+        }
+
+        [Test]
+        public void Test_SumOddOrEven_WithEvenSum()
+        {
+            string oddSum = Summator.SumOddOrEven(new int[] { 5, 7 });
+
+            string expected = "Sum result is even!";
+
+            Assert.AreEqual(expected, oddSum);
         }
     }
 }
--- Summator/Summator.cs
+++ Summator/Summator.cs
@@ -14,9 +14,11 @@
             return sum;
         }
 
-        public static int Average(int[] arr)
+        // Correct type of the variable sum and return type of method Average from int -> double, because:
+        // when dividing an odd result of sum by an even separator, the result is not an integer. 
+        public static double Average(int[] arr)
         {
-            int sum = 0;
+            double sum = 0;
 
             for (int i = 0; i < arr.Length; i++)
             {
@@ -24,6 +26,24 @@
             }
 
             return sum/arr.Length;
+        }
+
+        //Add new method for Summator. It`s calculate sum and return if is odd or even.
+        public static string SumOddOrEven(int[] arr)
+        {
+            long sum = 0;
+
+            foreach (var digit in arr)
+            {
+                sum += digit;
+            }
+
+            if (sum % 2 == 0)
+            {
+                return "Sum result is even!";
+            }
+
+            return "Sum result is odd!";
         }
     }
 }
